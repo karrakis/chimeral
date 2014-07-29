0c367bcef20b7ef605f1c75ac53a3c647cacca7b
ghuestis <ghuestis@communitydatanews.com>
2014-07-29 13:33:16 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chimeral::Application.config.secret_key_base = '4394d748edf816b46bcc6bf6fde46d9c82c28e6ab8e8de66e3d420ae2415b4a4d5f32e03c05e2ea07a33fd20bcda5b4d82a9997ca9f230e0d16ee1dbfeacad1e'
